3986e8f75fe6fbe7790073960470e9ae00d6e03d
Alex Baranowski <aleksander.baranowski@yahoo.pl>
2014-11-30 22:41:28 +0200

    "70% for command line done"

--- commandLine/resistor.h
+++ commandLine/resistor.h
@@ -1,14 +1,15 @@
 struct Resistor{
     /* This one shouldn't be changed after inicializaton */ 
     int  howMany;
-    char *first;
-    char *second;
-    char *third;
-    char *fourth;
-    char *fifth;
-    char *sixth;
-
+    char *first;    // First digi 
+    char *second;   // Second digit
+    char *third;    // Third digit 
+    char *fourth;   // Fourth is always multiplekser !!!
+    char *fifth;    // Tolrance
+    char *sixth;    // Six might be
+    /* this one should be changded*/
     double resistanceRounded;
     int baseResistance;
+    long long base; // if base is 0.1(-1) or 0.01(-2) 
     char *MultiPlekser;
 };
